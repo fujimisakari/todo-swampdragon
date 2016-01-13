65dc921511bf8ac58120d18c2542e9facd6e524e
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2016-01-14 04:22:29 +0900

    fix

--- application/settings.py
+++ application/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'application/app')],
+        'DIRS': [os.path.join(BASE_DIR, 'application/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,9 +99,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/app/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'application/app'),
+    os.path.join(BASE_DIR, 'application/static'),
 )
 
 # SwampDragon settings
